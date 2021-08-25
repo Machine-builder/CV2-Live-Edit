cf72e41e7450d50d8a7aaaf7e2bca5a65e53fa6c
Machine-builder <37518116+Machine-builder@users.noreply.github.com>
2021-08-25 11:29:20 +1000

    used os.path.getmtime to refresh filter file

--- main.py
+++ main.py
@@ -25,15 +25,30 @@ upscale = False
 display_error_messages = True
 error_message_colour = (255, 255, 255)
 
+# pick which file to run the filter
+# from - this can be any python file
+# but make sure to include the filter
+# start and filter end markers!
+filter_filename = 'filter.py'
+
 # ~====================~
 
 
 # just a default filter_image function
+# used so that the program doesn't crash
+# if there's no filter defined in the
+# filter file
 def filter_image(image): return image
 
 
+# track exceptions & save the last output
+# image, so that we can display exceptions
+# to the user without spamming them over
+# and over in the console
 last_exception = None
 last_output_image = None
+
+last_update_mtime = -1
 
 while 1:
     image = get_frame.get_frame()
@@ -41,30 +56,30 @@ while 1:
     if last_output_image is None:
         last_output_image = image
 
-    filter_code = open('filter.py', 'r').read().split(
-        '# start filter', 1)[-1].split(
-        '# end filter', 1)[0]
+    current_update_mtime = os.path.getmtime(filter_filename)
+    if current_update_mtime > last_update_mtime:
+        last_update_mtime = current_update_mtime
 
-    execution_valid = True
-
-    try:
-        exec(filter_code)
-        image_out = filter_image(image)
-        if image_out is not None:
-            last_output_image = image_out.copy()
-
-    except Exception as e:
-        this_exception = traceback.format_exc()
-        if last_exception != this_exception:
-            last_exception = this_exception
-            print()
-            print("  Exception")
-            print(" ~=========~")
-            print()
-            print(this_exception)
-
-        image_out = last_output_image
-        execution_valid = False
+        filter_code = open('filter.py', 'r').read().split(
+            '# start filter', 1)[-1].split(
+            '# end filter', 1)[0]
+        execution_valid = True
+        try:
+            exec(filter_code)
+            image_out = filter_image(image)
+            if image_out is not None:
+                last_output_image = image_out.copy()
+        except Exception as e:
+            this_exception = traceback.format_exc()
+            if last_exception != this_exception:
+                last_exception = this_exception
+                print()
+                print("  Exception")
+                print(" ~=========~")
+                print()
+                print(this_exception)
+            image_out = last_output_image
+            execution_valid = False
 
     try:
         if upscale:
@@ -85,30 +100,21 @@ while 1:
                 for line in write_lines:
                     if not line:
                         continue
-                    (w, h), b = cv2.getTextSize(line,
-                                                cv2.FONT_HERSHEY_COMPLEX_SMALL,
-                                                0.6,
-                                                1)
+                    (w, h), b = cv2.getTextSize(
+                        line, cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.6, 1)
                     w += 5
                     h += 6
                     if w > largest_w:
                         largest_w = w
-                cv2.rectangle(resized_image,
-                              (0, 20),
+                cv2.rectangle(resized_image, (0, 20),
                               (largest_w, 30+len(write_lines)*16),
-                              (0, 0, 0),
-                              -1)
-                line_index = 0
-                for line_index, line in enumerate(write_lines):
-                    line_index += 1
-                    x, y = (2, 16+16*line_index)
+                              (0, 0, 0), -1)
+                for index, line in enumerate(write_lines):
+                    x, y = (2, 32+16*index)
                     cv2.putText(resized_image,
-                                line,
-                                (x, y+5),
-                                cv2.FONT_HERSHEY_COMPLEX_SMALL,
-                                0.6,
-                                error_message_colour,
-                                1)
+                                line, (x, y+5),
+                                cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.6,
+                                error_message_colour, 1)
 
         cv2.imshow('Filtered Image', resized_image)
 
